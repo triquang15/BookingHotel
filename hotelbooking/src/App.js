d786df0174d6e255ab60466139d1de7413a8a716
qnguyen95 <quangtri.nguyen@dxc.com>
2023-02-21 11:02:20 +0700

    Code Header Component

--- hotelbooking/src/App.js
+++ hotelbooking/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import Explore from "./pages/Explore";
 import ForgotPassword from "./pages/ForgotPassword";
 import HomePage from "./pages/HomePage";
@@ -10,10 +11,11 @@ function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/account" element={<ProfilePage />} />
-          <Route path="/login" element={<SignIn/>} />
+          <Route path="/login" element={<SignIn />} />
           <Route path="/register" element={<Register />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/explore" element={<Explore />} />
---
+++ hotelbooking/src/components/Header.jsx
@@ -0,0 +1,60 @@
+import { useLocation, useNavigate } from "react-router-dom";
+export default function Header() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  function pathMathRoute(route) {
+    if (route === location.pathname) {
+      return true;
+    }
+  }
+  return (
+    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+      <header className="flex justify-between items-center px-3 max-w-8xl mx-auto">
+        <div>
+          <img
+            src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
+            alt="logo"
+            className="h-8 cursor-pointer"
+            onClick={() => navigate("/")}
+          />
+        </div>
+        <div>
+          <ul className="flex space-x-10">
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-red-400 border-b-[3px] border-b-transparent ${
+                pathMathRoute("/") && "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/")}
+            >
+              Home
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMathRoute("/explore") && "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/explore")}
+            >
+              Explore
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMathRoute("/login") && "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/login")}
+            >
+              Log in
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMathRoute("/login") && "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/register")}
+            >
+              Sign up
+            </li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+}
