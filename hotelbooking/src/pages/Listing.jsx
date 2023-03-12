61f4a917fe376ea59949c5ed43b0b36ac15d911b
qnguyen95 <quangtri.nguyen@dxc.com>
2023-03-12 20:52:33 +0700

    Link Copy

--- hotelbooking/src/pages/Listing.jsx
+++ hotelbooking/src/pages/Listing.jsx
@@ -6,13 +6,15 @@ import { db } from '../firebase';
 import {Swiper, SwiperSlide} from 'swiper/react';
 import SwiperCore, {EffectFade, Autoplay, Navigation, Pagination} from 'swiper';
 import 'swiper/css/bundle';
-
+import {FaShare} from 'react-icons/fa'
 export default function Listing() {
 
     const params = useParams();
     const [listing, setListing] = useState(null);
     const [loading, setLoading] = useState(true);
-    SwiperCore.use([Autoplay, Navigation, Pagination]);
+    const [shareLinkCopy, setShareLinkCopy] = useState(false)
+
+    SwiperCore.use([Autoplay, Navigation, Pagination]); 
 
     useEffect(() => {
         async function fetchListing() {
@@ -45,6 +47,21 @@ export default function Listing() {
                     </SwiperSlide>
                 ))}
             </Swiper>
+            <div className='fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer 
+            border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center'
+            onClick={() => {
+                navigator.clipboard.writeText(window.location.href)
+                setShareLinkCopy(true)
+                setTimeout(() => {
+                    setShareLinkCopy(false)
+                }, 2000)
+            }}>
+                <FaShare className='text-lg text-slate-500'/>
+            </div>
+            {shareLinkCopy && (
+                <p className='fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400
+                rounded-md bg-white z-10 p-2'>Link Copied</p>
+            )}
         </main>
     </div>
   )
